eb9847a0ae93cea95ad5f9aee6db94e7c02547b3
phaer <phaer@phaer.org>
2012-05-04 13:22:41 +0200

    updated xmonad config.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Actions.WindowGo
+import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
@@ -23,24 +24,28 @@ myPP :: PP
 myPP = defaultPP { ppCurrent = xmobarColor "#073642" "#eee8d5"
                  , ppHidden = xmobarColor "#fdf6e3" ""
                  , ppTitle   = xmobarColor "#fdf6e3" ""
-                 , ppVisible = xmobarColor "#073642" "#eee8d5" . wrap " (" ") " . trim
+                 , ppVisible = xmobarColor "#073642" "#eee8d5"
+                               . wrap " (" ") " . trim
                  , ppUrgent  = xmobarColor "#dc322f" ""
                  , ppSep = " | "
                  , ppWsSep = ""
                  }
-myWorkspaces = map (wrap " " " ") ["1:www", "2:shell", "3:emacs", "4:org", "5:mail", "6:music", "7:video", "8:pass", "9:nil"]
+myWorkspaces = map (wrap " " " ")
+               ["1:www", "2:shell", "3:emacs", "4:org", "5:mail", "6:music", 
+                "7:video", "8:pass", "9:nil"]
 
 myManageHook :: ManageHook
 myManageHook = composeAll
              [ (className =? "chromium") --> doShift "1:www"
-             , (title =? "shell") --> doShift "2:shell"
+             , (className =? "urxvt") --> doShift "2:shell"
              , (title =? "Agenda") --> doShift "4:org"
              , (className =? "Emacs") --> doShift "3:emacs"
              , (className =? "Icedove") --> doShift "5:mail"
              , (title =? "ncmpcpp") --> doShift "6:music"
              , (title =? "keepass") --> doShift "8:pass"
              , (className =? "mplayer") --> doShift "9:nil"
-             , isFullscreen --> doFullFloat]
+             , isFullscreen --> doFullFloat
+             , manageDocks]
 
 myLayoutHook = avoidStruts $ toggleLayouts (noBorders Full)
     (smartBorders (tiled ||| mosaic 2 [3,2] ||| Mirror tiled ))
@@ -56,17 +61,21 @@ main = do
      xmonad $ defaultConfig 
         { modMask               = mod4Mask
         , terminal              = "urxvt"
-	, startupHook = setWMName "LG3D" -- need that for broken java apps like minecraft.
         , normalBorderColor     = "#073642" -- base 02
         , focusedBorderColor    = "#eee8d5" -- base 2
-        , manageHook            = myManageHook <+> manageDocks <+> (manageHook defaultConfig) 
+        , manageHook            = myManageHook
+                                  <+> manageHook defaultConfig
         , layoutHook            = myLayoutHook
-        , logHook               = dynamicLogWithPP myPP { ppOutput  = hPutStrLn myXmobar}
+        , logHook               = dynamicLogWithPP myPP { ppOutput  = 
+                                                             hPutStrLn myXmobar}
         , workspaces            = myWorkspaces
 	}
         `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_l), spawn "slock")
         , ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c") 
-        , ((mod4Mask .|. shiftMask, xK_o), raiseMaybe (spawn "$HOME/code/bin/orgclient -c")                                                      (title =? "Agenda"))
-        , ((mod4Mask .|. shiftMask, xK_m), raiseMaybe (runInTerm "-title ncmpc" "ncmpcpp")
-                                                      (title =? "ncmpc")) ]
+        , ((mod4Mask .|. shiftMask, xK_o), raiseMaybe
+                                           (spawn "$HOME/code/bin/orgclient -c")
+                                           (title =? "Agenda"))
+        , ((mod4Mask .|. shiftMask, xK_m), raiseMaybe
+                                           (runInTerm "-title ncmpcpp" "ncmpcpp")
+                                           (title =? "ncmpcpp")) ]
